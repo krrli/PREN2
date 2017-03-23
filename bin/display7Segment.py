49838e8bc357fcac417d08b7ebe5dfd9ecce549d
krrli <carlaiten@gmail.com>
2017-03-23 11:35:24 +0100

    added digit on and off logic to RomanDisplay and debugScript display7segment

--- bin/display7Segment.py
+++ bin/display7Segment.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 import sys
+import time
 sys.path.append(".")  # make script callable from project directory
 sys.path.append("..")  # make script callable from bin directory
 
 from kamerad_schwungrad.RomanDisplay import RomanDisplay
 
-display = RomanDisplay()
-if len(sys.argv) < 2:
-    print("Please pass number to display as first command line argument\n\n")
-    sys.exit(1)
+inputInt = int(input("Enter a number to display: "))
+# print(inputInt)
 
-number_to_display = sys.argv[1]
-print("Displaying number", number_to_display + "\n")
-display.display_number(number_to_display)
+if inputInt > 5 or inputInt < 1:
+    print("dont. bigger than 5 or smaller than 1!", inputInt)
+
+display = RomanDisplay()
+
+number_to_display = inputInt
+print("Displaying number", inputInt)
+
+display.display_number(inputInt)
+time.sleep(5)
+
+
+display.cleanupAll()
--- kamerad_schwungrad/RomanDisplay.py
+++ kamerad_schwungrad/RomanDisplay.py
@@ -1,11 +1,68 @@
-
-
 """
 Responsible for displaying the roman digit on a 7 segment display.
 """
+import RPi.GPIO as GPIO
 class RomanDisplay:
+
+    def clearAllDigits(self):
+        # setup pins as OUT
+        for segment in self.allSegments:
+            print("segment auf 0: ", segment)
+            GPIO.setup(segment, 0)
+            # GPIO.cleanup(segment)
+
+    def cleanupAll(self):
+        for segment in self.allSegments:
+            GPIO.cleanup(segment)
+
     def __init__(self):
-        pass
+        self.allSegments = (7, 11, 13, 15, 29, 31, 33)
+        self.segments1 = (15, 33)
+        self.segments2 = (13, 15, 7, 29, 31)
+        self.segments3 = (13, 15, 7, 33, 31)
+        self.segments4 = (11, 7, 15, 33)
+        self.segments5 = (13, 11, 7, 33, 31)
+
+        # setMode of board
+        GPIO.setmode(GPIO.BOARD)
+
+        # setup pins as OUT
+        for segment in self.allSegments:
+            GPIO.setup(segment, GPIO.OUT)
+
+        self.clearAllDigits();
+
+
+    def printDigit(self, segments):
+        # self.clearAllDigits()
+        for segment in segments:
+            print("segmänt: ", segment)
+            GPIO.output(segment, 1)
+
+    def clearDigit(self, segments):
+        # wait for 1sec
+        # time.sleep(1)
+        for segment in segments:
+            print("clear digit")
+            GPIO.output(segment, 0)
 
     def display_number(self, number):
-        pass
+        if number == 1:
+            print("zeig s ", number, "\n")
+            self.printDigit(self.segments1)
+            # self.clearDigit(self.segments1)
+        if number == 2:
+            print("zeig s ", number, "\n")
+            self.printDigit(self.segments2)
+        if number == 3:
+            print("zeig s ", number, "\n")
+            self.printDigit(self.segments3)
+        if number == 4:
+            print("zeig s ", number, "\n")
+            self.printDigit(self.segments4)
+        if number == 5:
+            print("zeig s ", number, "\n")
+            self.printDigit(self.segments5)
+
+
+
