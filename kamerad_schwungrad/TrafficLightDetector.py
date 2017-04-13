71c81167f47c2ecd6ba963ddf3bfaa203a8cc158
Mogria <m0gr14@gmail.com>
2017-04-13 09:28:26 +0200

    add debug.py to only show image frame if ENABLE_DEBUG is set

--- kamerad_schwungrad/TrafficLightDetector.py
+++ kamerad_schwungrad/TrafficLightDetector.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from kamerad_schwungrad.debug import show_debug_frame, wait_for_input
 
 
 class TrafficLightDetector:
@@ -11,14 +12,13 @@ class TrafficLightDetector:
         return frame[:, 0:cut_width]
 
     def detect_red_traffic_light(self, frame):
-        return self.count_color(self.cut_frame(frame), [150, 100, 200], [200, 255, 255]) > 100
+        return self.count_color("red", self.cut_frame(frame), [150, 100, 200], [200, 255, 255]) > 100
 
     def detect_green_traffic_light(self, frame):
-        return self.count_color(self.cut_frame(frame), [35, 150, 145], [78, 255, 255]) > 100
+        return self.count_color("green", self.cut_frame(frame), [35, 150, 145], [78, 255, 255]) > 100
 
-    def count_color(self, frame, lower, upper):
-        ident = lower[0]
-        cv2.imshow('orig', frame)
+    def count_color(self, color, frame, lower, upper):
+        show_debug_frame('TrafficLight orig', frame)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         lower = np.array(lower, dtype="uint8")
@@ -28,10 +28,8 @@ class TrafficLightDetector:
         mask = cv2.inRange(hsv, lower, upper)
         res = cv2.bitwise_and(frame, frame, mask=mask)
 
-        cv2.imshow('maskcolor'+str(ident), mask)
-        cv2.imshow('rescolor'+str(ident), res)
+        show_debug_frame("TrafficLight " + color, res)
         countcolor = cv2.countNonZero(mask)
-        # while cv2.waitKey() & 0xFF == 'q':
-           # pass
+        # wait_for_input()
         print(ident, countcolor)
         return countcolor
---
+++ kamerad_schwungrad/debug.py
@@ -0,0 +1,14 @@
+
+import cv2
+
+ENABLE_DEBUG = True
+
+
+def show_debug_frame(window_title, frame):
+    if ENABLE_DEBUG:
+        cv2.imshow(window_title, frame)
+
+
+def wait_for_input():
+    while cv2.waitKey() & 0xFF == 'q':
+        pass
