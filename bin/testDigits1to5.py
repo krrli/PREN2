d3fc75304fc0f4f856ee273fd4a41f8f05169b5d
krrli <carlaiten@gmail.com>
2017-04-21 11:02:00 +0200

    changed segments

--- bin/testDigits1to5.py
+++ bin/testDigits1to5.py
@@ -4,24 +4,25 @@ import time
 # passt nonig so rechtig zu de mainbitch
 # bruchi github nur schnell as backup :D
 
-allSegments = (7,11,13,15,29,31,33)
-segments1 = (15, 33)
-segments2 = (13, 15, 7, 29, 31)
-segments3 = (13, 15, 7, 33, 31)
-segments4 = (11, 7, 15, 33)
-segments5 = (13, 11, 7, 33, 31)
+allSegments = (7,11,13,15,29,31,33, 35)
+segments1 = (33,31)
+segments2 = (29,33,7,11,15)
+segments3 = (29,33,7,31,15)
+segments4 = (13,7,33,31)
+segments5 = (29,13,7,31,15)
 
 # setMode of board
 GPIO.setmode(GPIO.BOARD)
 
 # setup pins as OUT
 for segment in allSegments:
+    # Setup
     GPIO.setup(segment, GPIO.OUT)
 
 def printDigit(segments, noToPrint):
-    for segment in segments:
-        print("digit: ", noToPrint)
-        GPIO.output(segment, 1)
+    # for segment in segments:
+    print("digit: ", noToPrint)
+    GPIO.output(segments, 1)
 
 
 def clearDigit(segments):
@@ -30,6 +31,7 @@ def clearDigit(segments):
     for segment in segments:
         print("clear digit")
         GPIO.output(segment, 0)
+
 
 printDigit(segments1, 1)
 clearDigit(segments1)
@@ -41,6 +43,7 @@ printDigit(segments4, 4)
 clearDigit(segments4)
 printDigit(segments5, 5)
 clearDigit(segments5)
+
 
 for segment in allSegments:
     GPIO.cleanup(segment)
