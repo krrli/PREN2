a6028d9e45f32f0cd0cb399c833f2380e6abe438
krrli <carlaiten@gmail.com>
2017-04-21 11:26:39 +0200

    added discolight

--- bin/testDigits1to5.py
+++ bin/testDigits1to5.py
@@ -10,6 +10,7 @@ segments2 = (29,33,7,11,15)
 segments3 = (29,33,7,31,15)
 segments4 = (13,7,33,31)
 segments5 = (29,13,7,31,15)
+segmentsJuhuu = (35,15,11,13,29,33,31)
 
 # setMode of board
 GPIO.setmode(GPIO.BOARD)
@@ -20,9 +21,9 @@ for segment in allSegments:
     GPIO.setup(segment, GPIO.OUT)
 
 def printDigit(segments, noToPrint):
-    # for segment in segments:
     print("digit: ", noToPrint)
-    GPIO.output(segments, 1)
+    for segment in segments:
+        GPIO.output(segments, 1)
 
 
 def clearDigit(segments):
@@ -32,7 +33,28 @@ def clearDigit(segments):
         print("clear digit")
         GPIO.output(segment, 0)
 
+def blinki(segments):
+    for segment in segments:
+        print("digit: ", segment)
+        GPIO.output(segment, 1)
+        time.sleep(1/8)
+        print("clear digit ", segment)
+        GPIO.output(segment, 0)
 
+c=15
+while (c > 0):
+    blinki(segmentsJuhuu)
+    if 0xFF == ord('q'):
+        break
+    c = c - 1
+
+
+'''
+blinki(segmentsJuhuu)
+blinki(segmentsJuhuu)
+blinki(segmentsJuhuu)
+'''
+print(" ----------- YOLO ----------- ")
 printDigit(segments1, 1)
 clearDigit(segments1)
 printDigit(segments2, 2)
