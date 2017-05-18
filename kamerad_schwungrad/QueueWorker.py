75a7daf730755b1ca2645c14411d59dbea267dfa
Mogria <m0gr14@gmail.com>
2017-05-18 10:59:20 +0200

    fix queue worker

--- kamerad_schwungrad/QueueWorker.py
+++ kamerad_schwungrad/QueueWorker.py
@@ -4,10 +4,13 @@ from threading import Event
 from queue import Queue
 from queue import Empty
 
+
 class QueueWorker:
+
     def __init__(self, romanNumeralDetector):
-        self._romanNumeralDetector = romanNumeralDetector
         self.number_detected = 0
+        self.idle = False
+        self._romanNumeralDetector = romanNumeralDetector
         self._queues_lock = Lock()
         self._frame_buffers = []
         self._stop_working_event = Event()
@@ -23,14 +26,18 @@ class QueueWorker:
     def _work_on_queues(self):
         while not self._stop_working_event.is_set():
             with self._queues_lock:
+                did_work = False
                 for frame_buffer in self._frame_buffers:
                     try:
                         frame = frame_buffer.frame_queue.get_nowait()
                         number_result = self._romanNumeralDetector.capture(frame)
                         if number_result != 0:
                             self.numberDetected = number_result
+                        did_work = True
                     except Empty:
                         pass
+
+                self._idle = not did_work
 
     def stop_capturing(self):
         self._stop_working_event.set()
